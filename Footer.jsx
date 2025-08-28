64bd80bcf6fa8c9a485ad1331505f82e2568c551
Michael Foisy <99748474+MichaelFoisy@users.noreply.github.com>
2025-08-28 11:50:20 -0400

    Create Footer.jsx

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <>
+     <footer>
+        <p>&copy; Event Planner Organization. All rights reserved.</p>
+    </footer>
+    </>
+  )
+}
