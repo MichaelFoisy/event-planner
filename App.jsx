5cfbf6f54e4efb0a753f73bd77e0c3c439edb614
Michael Foisy <99748474+MichaelFoisy@users.noreply.github.com>
2025-08-28 11:48:46 -0400

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import { EventPlanner } from './Components/EventPlanner'
+
+
+function App() {
+  
+
+  return (
+    <>
+    <EventPlanner />
+    </>
+  )
+}
+
+export default App
